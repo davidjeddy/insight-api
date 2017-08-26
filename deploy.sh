6c3d251489ea3a4b2417d3bf8c00a460c45deda9
David J Eddy <me@davidjeddy.com>
2017-08-26 14:55:40 -0400

    ADDED POSTman_collection UPDATED ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ docker-compose up --build -d
 docker stop db
 
 # run php dependency update
-docker exec -it  hackathonproject_app_1 wget https://getcomposer.org/composer.phar -O composer.phar
-docker exec -it  hackathonproject_app_1 php composer.phar install --ansi --profile --prefer-dist -o -vvv
+docker exec -it hackathonproject_app_1 wget https://getcomposer.org/composer.phar -O composer.phar
+docker exec -it hackathonproject_app_1 php composer.phar install --ansi --profile --prefer-dist -o -vvv
 
 # run migrations
 docker exec -it hackathonproject_app_1 php ./console/yii app/setup --interactive=0
