08f5481b373501a291d0921a7b1e62ea65845cc1
David J Eddy <me@davidjeddy.com>
2017-08-26 13:33:18 -0400

    UPDATED ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-git clean
+# repo steps
 git fetch origin
 git checkout devel
 git pull origin devel
+
+# build services, stop the db (using RDS)
 docker-compose up --build -d
 docker stop db
+
+# run migrations
 docker exec -it insight_app_1 php ./console/yii app/setup --interactive=0
 docker exec -it insight_app_1 php ./console/yii migrate/up --interactive=0
+
+# run php dependency update
+docker exec -it insight_app_1 php composer.phar update --ansi --profile --prefer-source -o -vvv
