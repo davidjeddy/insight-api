2dc81d27943f8573343b459e776745b05e5fa6b7
David J Eddy <me@davidjeddy.com>
2017-08-26 03:25:38 -0400

    ADDED logic to convert timestamp <-> datetime when req/res from classes derived from FE BaseCM

--- frontend/models/Base.php
+++ frontend/models/Base.php
@@ -15,5 +15,67 @@ use Yii;
  */
 class Base extends \yii\db\ActiveRecord
 {
+    /**
+     *
+     */
+    const RESPONSE_DATE_FORMAT = 'Y-m-d';
 
+    /**
+     *
+     */
+    public function afterFind()
+    {
+        parent::afterFind();
+
+        foreach ($this->getAttributes() as $key => $value)
+        {
+            if ($this->endsWith($value, '_date')) {
+                $this->setAttribute($key, date(self::RESPONSE_DATE_FORMAT, $value));
+            }
+        }
+    }
+
+    /**
+     * @param bool $insert
+     * @throws \Exception
+     */
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert)) {
+            throw new \Exception($this->getErrors());
+        }
+
+        foreach ($this->getAttributes() as $key => $value)
+        {
+            if ($this->endsWith($value, '_date')) {
+                $this->setAttribute($key, strtotime($value));
+            }
+        }
+    }
+
+    /**
+     * @source https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+     * @param $haystack
+     * @param $needle
+     * @return bool
+     */
+    public function startsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    /**
+     * @source https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+     * @param $haystack
+     * @param $needle
+     * @return bool
+     */
+    public function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+
+        return $length === 0 ||
+            (substr($haystack, -$length) === $needle);
+    }
 }
