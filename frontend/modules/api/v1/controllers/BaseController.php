4208a1f42bf5db17e21cde6b62c0f61af5feccd4
David J Eddy <me@davidjeddy.com>
2017-08-26 12:49:36 -0400

    Iterated on API BaseCNTL, removed un-needed logic

--- frontend/modules/api/v1/controllers/BaseController.php
+++ frontend/modules/api/v1/controllers/BaseController.php
@@ -1,11 +1,8 @@
 <?php
 namespace frontend\modules\api\v1\controllers;
 
-use Yii;
-use frontend\modules\api\v1\resources\Article;
 use yii\data\ActiveDataProvider;
 use yii\rest\ActiveController;
-use yii\web\ForbiddenHttpException;
 use yii\web\HttpException;
 
 /**
@@ -35,41 +32,12 @@ class BaseController extends ActiveController
         return [
             'index'   => [
                 'class'       => \yii\rest\IndexAction::class,
-                'modelClass'  => $this->modelClass,
-                'checkAccess' => [$this, 'checkAccess'],
+                'modelClass'  => $this->modelClass
             ],
             'view'    => [
                 'class'       => \yii\rest\ViewAction::class,
-                'modelClass'  => $this->modelClass,
-                'checkAccess' => [$this, 'checkAccess'],
+                'modelClass'  => $this->modelClass
             ],
         ];
-    }
-
-    /**
-     * @return ActiveDataProvider
-     */
-    public function prepareDataProvider()
-    {
-        return new ActiveDataProvider(array(
-            'query' => Article::find()->published()
-        ));
-    }
-
-    /**
-     * @param $id
-     * @return array|null|\yii\db\ActiveRecord
-     * @throws HttpException
-     */
-    public function findModel($id)
-    {
-        $model = Article::find()
-            ->published()
-            ->andWhere(['id' => (int) $id])
-            ->one();
-        if (!$model) {
-            throw new HttpException(404);
-        }
-        return $model;
-    }
+    }   
 }
